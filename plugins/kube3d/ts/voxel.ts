0ebc1788b6818dda88cf2c7148f5fa73acb47d3a
Stan Lewis <slewis@fusesource.com>
2015-07-23 13:49:03 -0400

    Add the ability to throw snowballs at pods to destroy them

--- plugins/kube3d/ts/voxel.ts
+++ plugins/kube3d/ts/voxel.ts
@@ -8,6 +8,7 @@ module Kube3d {
   export var VoxelController = controller('VoxelController', ['$scope', '$element', 'KubernetesModel', ($scope, $element, model:Kubernetes.KubernetesModelService) => {
 
     var creatures = {};
+    var bullets = [];
 
     var el = $element.find('.kube3d-control')[0];
     var game = createGame({
@@ -20,6 +21,11 @@ module Kube3d {
         container: el
       }, (game, avatar) => {
 
+        var bullet = new game.THREE.Mesh(new game.THREE.SphereGeometry(0.25, 32, 32), new game.THREE.MeshLambertMaterial({
+          color: 0xff0000
+        }));
+
+
         function newCreature(texture) {
           var THREE = game.THREE;
           var boxTexture = THREE.ImageUtils.loadTexture(texture);
@@ -31,7 +37,7 @@ module Kube3d {
             receiveShadow: true,
             wireframe: false
           });
-          var box = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), boxMaterial);
+          var box = new THREE.Mesh(new THREE.CubeGeometry(game.cubeSize, game.cubeSize, game.cubeSize), boxMaterial);
           return box;
         }
 
@@ -40,12 +46,14 @@ module Kube3d {
         game.flyer = makeFly(target);
 
         // highlight blocks when you look at them, hold <Ctrl> for block placement
+        /*
         var blockPosPlace, blockPosErase;
         var hl = game.highlighter = highlight(game, { color: 0xff0000 });
         hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos });
         hl.on('remove', function (voxelPos) { blockPosErase = null });
         hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos });
         hl.on('remove-adjacent', function (voxelPos) { blockPosPlace = null });
+        */
 
         // toggle between first and third person modes
         window.addEventListener('keydown', function (ev) {
@@ -56,14 +64,23 @@ module Kube3d {
         var currentMaterial = 1;
 
         game.on('fire', function (target, state) {
-          var position = blockPosPlace;
-          if (position) {
-            game.createBlock(position, currentMaterial);
+          log.debug("fire, target: ", target, " state: ", state);
+          if (bullets.length > 10) {
+            return;
           }
-          else {
-            position = blockPosErase
-              if (position) game.setBlock(position, 0);
-          }
+          var b = bullet.clone();
+          b.position.set(target.position.x, target.position.y + 1, target.position.z);
+          var direction = game.cameraVector();
+          var velocity = new game.THREE.Vector3(direction[0], direction[1], direction[2]);
+          velocity.multiplyScalar(0.05);
+          var item:any = {
+            mesh: b,
+            size: 0.8,
+            velocity: velocity
+          };
+          item = game.addItem(item);
+          log.debug("item: ", item);
+          bullets.push(item);
         });
 
         game.on('tick', function(delta) {
@@ -75,37 +92,97 @@ module Kube3d {
           } else {
             walk.startWalking()
           }
+          var toRemove = [];
+          _.forEach(bullets, (bullet) => {
+            if (bullet.lastPosition) {
+              var pos = bullet.mesh.position;
+              var last = bullet.lastPosition;
+              if (pos.x === last.x && pos.y === last.y && pos.z === last.z) {
+                game.removeItem(bullet);
+                toRemove.push(bullet);
+              }
+            }
+            bullet.lastPosition = {
+              x: bullet.mesh.position.x,
+              y: bullet.mesh.position.y,
+              z: bullet.mesh.position.z
+            }
+            var bulletAABB = bullet.aabb();
+            var hit = false;
+            _.forIn(creatures, (creature, key) => {
+              if (!creature.entity || hit) {
+                return;
+              }
+              if (bulletAABB.intersects(creature.entity.aabb())) {
+                hit = true;
+                game.removeItem(bullet);
+                toRemove.push(bullet);
+                creature.hit = true;
+              }
+            });
+          });
+          _.forEach(toRemove, (bullet) => {
+            _.remove(bullets, bullet);
+          });
           if (game.pendingChunks.length) {
             log.debug("Pending chunks, skipping entity creation");
             return;
           }
+          var creaturesToRemove = [];
           _.forIn(creatures, (creature, key) => {
+            if (creature.hit && !creature.deleted) {
+              model['podsResource'].delete({ id: Kubernetes.getName(creature.pod) });
+              creature.deleted = true;
+              if (creature.clearInterval) {
+                creature.clearInterval();
+                delete creature.clearInterval;
+              }
+              creature.entity.velocity.y = 2;
+              creature.entity.resting = false;
+              log.debug("Creature hit: ", creature);
+            }
+            if (creature.hit && creature.entity) {
+              creature.entity.mesh.scale.x = creature.entity.mesh.scale.x + 0.05;
+              creature.entity.mesh.scale.z = creature.entity.mesh.scale.z + 0.05;
+            }
             if (!(key in model.podsByKey)) {
               log.debug("need to delete creature ", key);
-              if (creature.entity) {
-                game.removeItem(creature.entity);
-                if (creature.clearInterval) {
-                  creature.clearInterval();
-                }
-              }
+              creaturesToRemove.push(key);
             }
             if (!('entity' in creature)) {
               log.debug("need to create creature ", key);
               var mesh = newCreature(creature.pod.$iconUrl);
+              mesh.name = key;
               mesh.position.set(Math.random() * 20 - 10, 30, Math.random() * 20 - 10);
               var item:any = {
                 mesh: mesh,
-                size: 2,
-                velocity: { x: 0, y: 0.9, z: 0 }
+                size: 1,
+                velocity: { x: 0, y: 0, z: 0 }
               };
               creature.entity = game.addItem(item);
               creature.clearInterval = game.setInterval(() => {
+                if (creature.deleted) {
+                  return;
+                }
                 creature.entity.velocity.x = (Math.random() * 10 - 5) * 0.005;
                 creature.entity.velocity.z = (Math.random() * 10 - 5) * 0.005;
                 creature.entity.velocity.y = (Math.random() * 10 - 5) * 0.005;
                 creature.entity.resting = false;
               }, Math.random() * 5000 + 500);
             }
+          });
+          _.forEach(creaturesToRemove, (key) => {
+            var creature = creatures[key];
+            if (!creature) {
+              return;
+            }
+            if (creature.entity) {
+              game.removeItem(creature.entity);
+              if (creature.clearInterval) {
+                creature.clearInterval();
+              }
+            }
+            delete creatures[key];
           });
         });
       });
