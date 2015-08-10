e9a41d09a1b29c984be756a579af61075f46683c
Stan Lewis <slewis@fusesource.com>
2015-08-10 13:14:08 -0400

    Stick a title screen on there too

--- defs.d.ts
+++ defs.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="d.ts/includes.d.ts"/>
 /// <reference path="d.ts/kube3d/ts/kube3dInterfaces.d.ts"/>
 /// <reference path="d.ts/kube3d/ts/kube3dHelpers.d.ts"/>
 /// <reference path="d.ts/kube3d/ts/sounds.d.ts"/>
